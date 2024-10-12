9a1e77eba26704ad02bef8e32d5fad1c6b0a3965
boothinator <boothinator@users.noreply.github.com>
2024-10-12 16:31:52 -0400

    Analyze combos

---
+++ 4-analyze_all_combos.py
@@ -0,0 +1,28 @@
+
+import json
+
+
+with open(f"combinations.json") as f:
+  combos = json.load(f)
+
+lowest_max_tooth = 35
+lowest_chain_wrap = 40
+
+lowest_gearing_with_drop_bar_shifters_combos = []
+widest_range_with_drop_bar_shifters_combos = []
+
+for combo in combos:
+  if combo["shifterType"] == "drop-bar" and combo["moreCogsThanShifts"] == False:
+    if combo["maxTooth"] >= lowest_max_tooth:
+      lowest_gearing_with_drop_bar_shifters_combos.append(combo)
+    if combo["chainWrap"] >= lowest_chain_wrap:
+      widest_range_with_drop_bar_shifters_combos.append(combo)
+
+lowest_gearing_with_drop_bar_shifters_combos.sort(key=lambda combo: combo["maxTooth"], reverse=True)
+widest_range_with_drop_bar_shifters_combos.sort(key=lambda combo: combo["chainWrap"], reverse=True)
+
+with open(f"lowest_gearing_with_drop_bar_shifters_combos.json", "w") as info_file:
+  json.dump(lowest_gearing_with_drop_bar_shifters_combos, info_file, indent=2)
+
+with open(f"widest_range_with_drop_bar_shifters_combos.json", "w") as info_file:
+  json.dump(widest_range_with_drop_bar_shifters_combos, info_file, indent=2)
