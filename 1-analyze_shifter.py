18c1aae42755955ac3413f188eafdca240f0b994
boothinator <boothinator@users.noreply.github.com>
2024-10-13 10:22:07 -0400

    Template and improvements

--- 1-analyze_shifter.py
+++ 1-analyze_shifter.py
@@ -4,19 +4,30 @@ import matplotlib.pyplot as plt
 import os
 import json
 import math
+import re
 
 def analyze(input_file):
   data = []
+  row_headers = []
 
   with open(input_file, newline='') as csvfile: 
     reader = csv.reader(csvfile)
     for row in reader:
       data.append(row)
+      row_headers.append(row[0])
+  
+  # Find where measurements tart
+  gear_headers = [(i, h, m[1]) for i, h, m in 
+    [(i,h,re.match(r'^(\d{1,2}).*', h)) for i,h in enumerate(row_headers)]
+    if m
+  ]
+  first_gear_row_index = min([i for i,_,_ in gear_headers])
+  last_gear_row_index = max([i for i,_,_ in gear_headers])
   
   # Extract just the measurements and convert to numbers
   measurement_data = np.array([
     [(float(c) if len(c) > 0 else float('nan')) for c in row[1:]]
-    for row in data[4:]
+    for row in data[first_gear_row_index:last_gear_row_index+1]
   ])
 
   # TODO: normalize data between runs
@@ -49,6 +60,13 @@ def analyze(input_file):
 
 
 for dir in os.listdir('shifters'):
+  if dir == "template":
+    continue
+  
+  #FIXME:TESTING
+  if dir != "Microshift Advent X":
+    continue
+
   with open(f"shifters/{dir}/info.json") as info_file:
     info = json.load(info_file)
   
