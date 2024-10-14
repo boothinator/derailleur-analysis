bbc8fda9d7eb8c2bc1ffc52784d48fc744a7e06e
boothinator <boothinator@users.noreply.github.com>
2024-10-14 11:51:14 -0400

    More charts

--- 1-analyze_shifter.py
+++ 1-analyze_shifter.py
@@ -67,9 +67,9 @@ def analyze(input_file, out_folder):
 
   # Plot Averages
 
-  gear_averages = df.groupby(["Gear"])["NormalizedMeasurement"].mean().rename("Average")
-  pulling_gear_averages = df[df["Direction"] == 'Pulling'].groupby(["Gear"])["NormalizedMeasurement"].mean().rename("Average Pulling")
-  relaxing_gear_averages = df[df["Direction"] == 'Relaxing'].groupby(["Gear"])["NormalizedMeasurement"].mean().rename("Average Relaxing")
+  gear_averages = df.groupby(["Gear"], sort=False)["NormalizedMeasurement"].mean().rename("Average")
+  pulling_gear_averages = df[df["Direction"] == 'Pulling'].groupby(["Gear"], sort=False)["NormalizedMeasurement"].mean().rename("Average Pulling")
+  relaxing_gear_averages = df[df["Direction"] == 'Relaxing'].groupby(["Gear"], sort=False)["NormalizedMeasurement"].mean().rename("Average Relaxing")
 
   avgs = pd.DataFrame([gear_averages, pulling_gear_averages, relaxing_gear_averages]).T
 
@@ -80,9 +80,9 @@ def analyze(input_file, out_folder):
 
   # Plot Stdev
 
-  gear_stdev = df.groupby(["Gear"])["NormalizedMeasurement"].std().rename("Std Dev")
-  pulling_gear_stdev = df[df["Direction"] == 'Pulling'].groupby(["Gear"])["NormalizedMeasurement"].std().rename("Std Dev Pulling")
-  relaxing_gear_stdev = df[df["Direction"] == 'Relaxing'].groupby(["Gear"])["NormalizedMeasurement"].std().rename("Std Dev Relaxing")
+  gear_stdev = df.groupby(["Gear"], sort=False)["NormalizedMeasurement"].std().rename("Std Dev")
+  pulling_gear_stdev = df[df["Direction"] == 'Pulling'].groupby(["Gear"], sort=False)["NormalizedMeasurement"].std().rename("Std Dev Pulling")
+  relaxing_gear_stdev = df[df["Direction"] == 'Relaxing'].groupby(["Gear"], sort=False)["NormalizedMeasurement"].std().rename("Std Dev Relaxing")
 
   avgs = pd.DataFrame([gear_stdev, pulling_gear_stdev, relaxing_gear_stdev]).T
 
@@ -91,8 +91,7 @@ def analyze(input_file, out_folder):
   plt.savefig(f"{out_folder}/meas_stdev.png")
 
   # Diff between relaxing and pulling averages
-  relaxing_pulling_diffs = df[df["Direction"] == "Relaxing"].groupby(["Gear"])["NormalizedMeasurement"].mean() \
-                          - df[df["Direction"] == "Pulling"].groupby(["Gear"])["NormalizedMeasurement"].mean()
+  relaxing_pulling_diffs = relaxing_gear_averages - pulling_gear_averages
 
   plt.clf()
   relaxing_pulling_diffs.plot.bar()
@@ -103,7 +102,7 @@ def analyze(input_file, out_folder):
   # Besides, here we only care about differences, not absolute values
   md = df.groupby(["Run"], sort=False)
 
-  gear_step_df = md.apply(lambda d: pd.DataFrame(dict(
+  shifts_df = md.apply(lambda d: pd.DataFrame(dict(
     [(header, d[header].iloc[:-1])
      for header in row_headers[:first_gear_row_index]
      if header != "Run"]+
@@ -115,20 +114,49 @@ def analyze(input_file, out_folder):
 
   #TODO: diff between pulling and relaxing, average pulling, average relaxing, 95% confidence interval
 
-  gb = gear_step_df.groupby(["GearStep"], sort=False)["Shift"].mean()
+  # Plot Shift Averages
 
-  # Calculate averages
-  averages = gear_step_df.groupby(["GearStep"], sort=False)["Shift"].mean()
-  stdev = gear_step_df.groupby(["GearStep"], sort=False)["Shift"].std()
-  print(averages[1:-1])
-  cable_pull = np.mean(averages[1:-1])
+  shift_averages = shifts_df.groupby(["GearStep"], sort=False)["Shift"].mean()
+  pulling_shift_averages = shifts_df[shifts_df["Direction"] == 'Pulling'].groupby(["GearStep"], sort=False)["Shift"].mean().rename("Average Pulling")
+  relaxing_shift_averages = shifts_df[shifts_df["Direction"] == 'Relaxing'].groupby(["GearStep"], sort=False)["Shift"].mean().rename("Average Relaxing")
+
+  shift_avgs_df = pd.DataFrame([shift_averages, pulling_shift_averages, relaxing_shift_averages]).T
+
+  plt.clf()
+  shift_avgs_df.plot.bar()
+  plt.savefig(f"{out_folder}/shift_avgs.png")
+
+  # Plot Shift Std Dev
+
+  shift_stdev = shifts_df.groupby(["GearStep"], sort=False)["Shift"].std()
+  pulling_shift_stdev = shifts_df[shifts_df["Direction"] == 'Pulling'].groupby(["GearStep"], sort=False)["Shift"].std().rename("Average Pulling")
+  relaxing_shift_stdev = shifts_df[shifts_df["Direction"] == 'Relaxing'].groupby(["GearStep"], sort=False)["Shift"].std().rename("Average Relaxing")
+
+  shift_stdevs_df = pd.DataFrame([shift_stdev, pulling_shift_stdev, relaxing_shift_stdev]).T
+
+  plt.clf()
+  shift_stdevs_df.plot.bar()
+  plt.savefig(f"{out_folder}/shift_stdev.png")
+
+  # Plot Shift Differences
+
+  shift_relaxing_pulling_diffs = relaxing_shift_averages - pulling_shift_averages
+
+  plt.clf()
+  shift_relaxing_pulling_diffs.plot.bar()
+  plt.savefig(f"{out_folder}/shift_diffs.png")
+  
+  ###
+
+  print(shift_averages[1:-1])
+  cable_pull = np.mean(shift_averages[1:-1])
 
   print(cable_pull)
 
   # TODO: generate cable pull graph
 
   return {
-    "shiftSpacings": averages.to_list(),
+    "shiftSpacings": shift_averages.to_list(),
     "cablePull": cable_pull
   }
   
