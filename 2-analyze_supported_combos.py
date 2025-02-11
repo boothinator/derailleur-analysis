254fe8a6b931a59d48348cb32791f20cd8b8fd49
boothinator <boothinator@users.noreply.github.com>
2025-02-11 08:39:05 -0500

    Update analysis script

--- 2-analyze_supported_combos.py
+++ 2-analyze_supported_combos.py
@@ -13,27 +13,22 @@ with open(f"cassettes.json") as f:
 with open(f"supported_combinations.json") as f:
   supported_combos = json.load(f)
 
-names = []
-motion_multipliers = []
-max_chain_angles = []
-
 for combo in supported_combos:
   shifter = [s for s in shifters
-             if s["partNumber"] == combo["shifterPartNumber"]
-             and s["brand"] == combo["brand"]][0]
-  derailleur = [s for s in derailleurs if s["partNumber"] == combo["derailleurPartNumber"]
-             and s["brand"] == combo["brand"]][0]
-  cassette = [s for s in cassettes if s["partNumber"] == combo["cassettePartNumber"]
-             and (s["brand"] == combo["brand"] or
-                  ("cassetteBrand" in combo and s["brand"] == combo["cassetteBrand"]))][0]
+             if s["partNumber"] == combo["shifterPartNumber"]][0]
+  derailleur = [s for s in derailleurs if s["partNumber"] == combo["derailleurPartNumber"]][0]
+  cassette = [s for s in cassettes if s["partNumber"] == combo["cassettePartNumber"]][0]
+
+  combo["shifter"] = shifter
+  combo["derailleur"] = derailleur
+  combo["cassette"] = cassette
 
   motion_multiplier = cassette["averagePitch"] / (shifter["cablePull"] * derailleur["pullRatio"])
 
-  names.append(f"{combo['name']}")
-  motion_multipliers.append(motion_multiplier)
+  combo["motion_multiplier"] = motion_multiplier
 
   max_chain_angle_results = calculate_max_chain_angle(shifter, derailleur, cassette)
-  max_chain_angles.append(max_chain_angle_results["max_chain_angle"])
+  combo["max_chain_angle"] = max_chain_angle_results["max_chain_angle"]
 
   if max_chain_angle_results["barrel_adjuster_too_low"]:
     raise Exception("Barrel adjuster too low")
@@ -47,62 +42,205 @@ for combo in supported_combos:
   if max_chain_angle_results["derailleur_can_clear_cassette"] == False:
     print(f"Warning: derailleur can't clear cassette for {combo['name']}")
 
+names = [c["name"] for c in supported_combos]
+motion_multipliers = [c["motion_multiplier"] for c in supported_combos]
+max_chain_angles = [c["max_chain_angle"] for c in supported_combos]
 
 motion_multiplier_avg = np.mean(motion_multipliers)
 # Perhaps I should use the sample standard deviation (ddof=1), but using 
 # regular standard deviation is more conservative
 motion_multiplier_stdev = np.std(motion_multipliers)
-motion_multiplier_min = motion_multiplier_avg - 2*motion_multiplier_stdev
-motion_multiplier_max = motion_multiplier_avg + 2*motion_multiplier_stdev
+motion_multiplier_num_stdevs = (max(motion_multipliers) - motion_multiplier_avg) / motion_multiplier_stdev + 0.00001
+motion_multiplier_min = motion_multiplier_avg - motion_multiplier_num_stdevs*motion_multiplier_stdev
+motion_multiplier_max = motion_multiplier_avg + motion_multiplier_num_stdevs*motion_multiplier_stdev
+
+mm_dist = scipy.stats.norm(motion_multiplier_avg, motion_multiplier_stdev)
+motion_multiplier_min_confidence = 1 - (mm_dist.cdf(motion_multiplier_max) - mm_dist.cdf(motion_multiplier_min))
 
 max_chain_angle_avg = np.mean(max_chain_angles)
 max_chain_angle_stdev = np.std(max_chain_angles)
-max_chain_angle_max = max_chain_angle_avg + 2*max_chain_angle_stdev
+max_chain_angle_num_stdevs = (max(max_chain_angles) - max_chain_angle_avg) / max_chain_angle_stdev + 0.00001
+max_chain_angle_max = max_chain_angle_avg + max_chain_angle_num_stdevs*max_chain_angle_stdev
 
 # Validate that all supported combos are in range
-out_of_range = [supported_combos[i]["name"] for (i, mm) in enumerate(motion_multipliers)
-                if mm < motion_multiplier_min or motion_multiplier_max < mm
-                  or max_chain_angles[i] > max_chain_angle_max]
+motion_multiplier_out_of_range = [supported_combos[i]["name"] for (i, mm) in enumerate(motion_multipliers)
+                if mm < motion_multiplier_min or motion_multiplier_max < mm]
+max_chain_angle_out_of_range = [supported_combos[i]["name"] for (i, mm) in enumerate(max_chain_angles)
+                if max_chain_angles[i] > max_chain_angle_max]
 
-if len(out_of_range) > 0:
-  print("out of range: ", out_of_range)
-else:
+if len(motion_multiplier_out_of_range) > 0:
+  print("Motion multiplier out of range: ", motion_multiplier_out_of_range)
+
+if len(max_chain_angle_out_of_range) > 0:
+  print("Max chain angle out of range: ", max_chain_angle_out_of_range)
+
+if not motion_multiplier_out_of_range and not max_chain_angle_out_of_range:
   print("All supported combos are in range")
 
 compatibility_ranges = {
   "motionMultiplierAvg": motion_multiplier_avg,
   "motionMultiplierStdev": motion_multiplier_stdev,
+  "motionMultiplierNumStdevs": motion_multiplier_num_stdevs,
   "motionMultiplierMin": motion_multiplier_min,
   "motionMultiplierMax": motion_multiplier_max,
   "motionMultiplierMinObserved": min(motion_multipliers),
   "motionMultiplierMaxObserved": max(motion_multipliers),
+  "motionMultiplierMinConfidence": motion_multiplier_min_confidence,
   "maxChainAngleAvg": max_chain_angle_avg,
   "maxChainAngleStdev": max_chain_angle_stdev,
+  "maxChainAngleNumStdevs": max_chain_angle_num_stdevs,
   "maxChainAngleMax": max_chain_angle_max,
   "maxChainAngleMaxObserved": max(max_chain_angles),
   "groups": names,
-  "groupMotionMultipliers": motion_multipliers
+  "groupMotionMultipliers": motion_multipliers,
+  "groupMaxChainAngles": max_chain_angles
 }
 
 with open(f"compatibility_ranges.json", "w") as info_file:
   json.dump(compatibility_ranges, info_file, indent=2)
 
+prefixes = []
+max_chain_angle_avgs = []
+motion_multiplier_avgs = []
+max_chain_angle_stdevs = []
+motion_multiplier_stdevs = []
+counts = []
+
+def graph_combos(prefix, combos):
+
+  names = [c["name"] for c in combos]
+  motion_multipliers = [c["motion_multiplier"] for c in combos]
+  max_chain_angles = [c["max_chain_angle"] for c in combos]
+
+  motion_multiplier_avg = np.mean(motion_multipliers)
+  # Perhaps I should use the sample standard deviation (ddof=1), but using 
+  # regular standard deviation is more conservative
+  motion_multiplier_stdev = np.std(motion_multipliers)
+  motion_multiplier_num_stdevs = 2
+  motion_multiplier_min = motion_multiplier_avg - motion_multiplier_num_stdevs*motion_multiplier_stdev
+  motion_multiplier_max = motion_multiplier_avg + motion_multiplier_num_stdevs*motion_multiplier_stdev
+
+  max_chain_angle_avg = np.mean(max_chain_angles)
+  max_chain_angle_stdev = np.std(max_chain_angles)
+  max_chain_angle_num_stdevs = 2
+  max_chain_angle_max = max_chain_angle_avg + max_chain_angle_num_stdevs*max_chain_angle_stdev
+  max_chain_angle_min = max(max_chain_angle_avg - max_chain_angle_num_stdevs*max_chain_angle_stdev, 0)
+
+  prefixes.append(prefix)
+  max_chain_angle_avgs.append(max_chain_angle_avg)
+  motion_multiplier_avgs.append(motion_multiplier_avg)
+  max_chain_angle_stdevs.append(max_chain_angle_stdev)
+  motion_multiplier_stdevs.append(motion_multiplier_stdev)
+  counts.append(len(combos))
+
+  plt.clf()
+  plt.bar(names, motion_multipliers, color="purple")
+  plt.plot(names,[motion_multiplier_avg]*len(names),
+          names,[motion_multiplier_min]*len(names),
+          names,[motion_multiplier_max]*len(names))
+  plt.xticks(wrap=False, rotation="vertical", rotation_mode="anchor",
+            horizontalalignment="center", verticalalignment="top")
+  plt.tight_layout()
+  plt.ylim(motion_multiplier_min - motion_multiplier_stdev, motion_multiplier_max + motion_multiplier_stdev)
+  plt.savefig(f"combo_analysis/{prefix}motion_multiplier.png")
+
+
+  curve = scipy.stats.norm(motion_multiplier_avg, motion_multiplier_stdev)
+  x = np.linspace(motion_multiplier_min, motion_multiplier_max)
+
+  plt.clf()
+  plt.hist(motion_multipliers)
+  plt.plot(x, curve.pdf(x)/10)
+  plt.savefig(f"combo_analysis/{prefix}motion_multiplier_histogram.png")
+
+
+  plt.clf()
+  plt.bar(names, max_chain_angles, color="purple")
+  plt.plot(names,[max_chain_angle_avg]*len(names),
+          names,[max_chain_angle_min]*len(names),
+          names,[max_chain_angle_max]*len(names))
+  plt.xticks(wrap=False, rotation="vertical", rotation_mode="anchor",
+            horizontalalignment="center", verticalalignment="top")
+  plt.tight_layout()
+  plt.ylim(0, max_chain_angle_max + max_chain_angle_stdev)
+  plt.savefig(f"combo_analysis/{prefix}max_chain_angle.png")
+
+
+  curve = scipy.stats.norm(max_chain_angle_avg, max_chain_angle_stdev)
+  x = np.linspace(max_chain_angle_min, max_chain_angle_max)
+
+  plt.clf()
+  plt.hist(max_chain_angles)
+  plt.plot(x, curve.pdf(x)/10)
+  plt.savefig(f"combo_analysis/{prefix}max_chain_angle_histogram.png")
+
+graph_combos("all_", supported_combos)
+graph_combos("10_speed_", [c for c in supported_combos if c["shifter"]["speeds"] == 10 and "LinkGlide" not in c["cassette"]["name"]])
+graph_combos("11_speed_", [c for c in supported_combos if c["shifter"]["speeds"] == 11 and "LinkGlide" not in c["cassette"]["name"]])
+graph_combos("linkglide_", [c for c in supported_combos if "LinkGlide" in c["cassette"]["name"]])
+#graph_combos("slant_", [c for c in supported_combos if c["derailleur"]["parallelogramStyle"] == "slant"])
+#graph_combos("straight_", [c for c in supported_combos if c["derailleur"]["parallelogramStyle"] == "straight"])
+graph_combos("34t_or_less_", [c for c in supported_combos if c["derailleur"]["maxTooth"] <= 34])
+graph_combos("34t_through_42t_", [c for c in supported_combos if c["derailleur"]["maxTooth"] > 34 and c["derailleur"]["maxTooth"] <= 42])
+graph_combos("more_than_42t_", [c for c in supported_combos if c["derailleur"]["maxTooth"] > 42])
+
+
+
+
 plt.clf()
-plt.bar(names, motion_multipliers, color="purple")
-plt.plot(names,[motion_multiplier_avg]*len(names),
-         names,[motion_multiplier_min]*len(names),
-         names,[motion_multiplier_max]*len(names))
+plt.bar(prefixes, motion_multiplier_avgs, color="purple")
 plt.xticks(wrap=False, rotation="vertical", rotation_mode="anchor",
-           horizontalalignment="center", verticalalignment="top")
+          horizontalalignment="center", verticalalignment="top")
+plt.ylim(min(motion_multiplier_avgs) - 0.01, max(motion_multiplier_avgs) + 0.01)
 plt.tight_layout()
-plt.ylim(motion_multiplier_min - motion_multiplier_stdev, motion_multiplier_max + motion_multiplier_stdev)
-plt.savefig(f"motion_multiplier.png")
+plt.savefig(f"combo_analysis/category_motion_multiplier_avg.png")
 
+plt.clf()
+plt.bar(prefixes, max_chain_angle_avgs, color="purple")
+plt.xticks(wrap=False, rotation="vertical", rotation_mode="anchor",
+          horizontalalignment="center", verticalalignment="top")
+plt.tight_layout()
+plt.savefig(f"combo_analysis/category_max_chain_angle_avg.png")
 
-curve = scipy.stats.norm(motion_multiplier_avg, motion_multiplier_stdev)
+plt.clf()
+plt.bar(prefixes, motion_multiplier_stdevs, color="purple")
+plt.xticks(wrap=False, rotation="vertical", rotation_mode="anchor",
+          horizontalalignment="center", verticalalignment="top")
+plt.tight_layout()
+plt.savefig(f"combo_analysis/category_motion_multiplier_stdev.png")
+
+plt.clf()
+plt.bar(prefixes, max_chain_angle_stdevs, color="purple")
+plt.xticks(wrap=False, rotation="vertical", rotation_mode="anchor",
+          horizontalalignment="center", verticalalignment="top")
+plt.tight_layout()
+plt.savefig(f"combo_analysis/category_max_chain_angle_stdev.png")
+
+plt.clf()
+plt.bar(prefixes, counts, color="purple")
+plt.xticks(wrap=False, rotation="vertical", rotation_mode="anchor",
+          horizontalalignment="center", verticalalignment="top")
+plt.tight_layout()
+plt.savefig(f"combo_analysis/category_counts.png")
+
+plt.clf()
 x = np.linspace(motion_multiplier_min, motion_multiplier_max)
 
+for i,prefix in enumerate(prefixes):
+  curve = scipy.stats.norm(motion_multiplier_avgs[i], motion_multiplier_stdevs[i])
+  plt.plot(x, curve.pdf(x)/10, label=prefix)
+
+plt.legend()
+
+plt.savefig(f"combo_analysis/motion_multiplier_dists.png")
+
 plt.clf()
-plt.hist(motion_multipliers)
-plt.plot(x, curve.pdf(x)/10)
-plt.savefig(f"motion_multiplier_histogram.png")
+x = np.linspace(0, max_chain_angle_max)
+
+for i,prefix in enumerate(prefixes):
+  curve = scipy.stats.norm(max_chain_angle_avgs[i], max_chain_angle_stdevs[i])
+  plt.plot(x, curve.pdf(x)/10, label=prefix)
+
+plt.legend()
+
+plt.savefig(f"combo_analysis/max_chain_angle_dists.png")
